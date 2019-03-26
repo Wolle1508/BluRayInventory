4dbdb516dbfc8409c640d1e6f81753706107b211
Niklas Wollburg <nikwol@online.de>
2019-03-26 22:38:36 +0100

    reimplemented config

--- src/Utils/ConfigUtils.js
+++ src/Utils/ConfigUtils.js
@@ -1,16 +1,45 @@
 function renderConfigModal() {
      document.getElementById("url").value = config.url;
-     document.getElementById("themeConf").value = config.themeConf;
+     var div = document.getElementById("themeDiv");
+     div.innerHTML = "";
+     var label = document.createElement("label");
+     label.className = "switch";
+     var input = document.createElement("input");
+     input.type = "checkbox";
+     input.id = "themeVal";
+     if (config.themeConf == "dark") {
+          input.checked = "true";
+     }
+     input.addEventListener("change", function () {
+          var oldlink = document.getElementById("theme");
+          var newlink = document.createElement("link");
+          newlink.setAttribute("rel", "stylesheet");
+          newlink.setAttribute("type", "text/css");
+          newlink.setAttribute("id", "theme");
+          if (input.checked == false) {
+               newlink.setAttribute("href", "css/light.css");
+          } else {
+               newlink.setAttribute("href", "css/dark.css");
+          }
+          document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+     });
+     label.appendChild(input);
+     var span = document.createElement("SPAN");
+     span.className = "slider round";
+     label.appendChild(span);
+     div.appendChild(label);
 }
 
 function saveSettings() {
      let _inputs = document.getElementById("settingsForm");
-     for (const input in _inputs) {
-          if (_inputs.hasOwnProperty(input)) {
-               config[_inputs[input].id] = _inputs[input].value;
-          }
+     let _url = document.getElementById("url").value;
+     let _theme = document.getElementById("themeVal").checked;
+     config.url = _url;
+     if (_theme == false) {
+          config.themeConf = "light";
+     } else {
+          config.themeConf = "dark";
      }
-
      fs.writeFile("config.json", JSON.stringify(config), (err) => {
           if (err) throw err;
           AlertUtils.callSuccessAlert("Settings have been saved!");
